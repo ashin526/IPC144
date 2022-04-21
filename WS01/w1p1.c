b8f6bdfe0a6bd6a80dedf64757a52eaf21ef1a90
Anna Shin <anna.shin526@gmail.com>
2022-04-20 20:03:25 -0400

    Workshop 01

---
+++ WS01/w1p1.c
@@ -0,0 +1,16 @@
+/*
+======================================================
+workshop #1 (Part-1)
+======================================================
+Name    : Anna Shin
+ID      : 131283210
+Email   : ashin8@myseneca.ca
+Section : NHH
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main(){
+    printf("*** Welcome to Seneca's C Programming Course ***\n");
+    return 0;
+}
---
+++ WS01/w1p2.c
@@ -0,0 +1,24 @@
+/*
+======================================================
+workshop #1 (Part-2)
+======================================================
+Name    : Anna Shin
+ID      : 131283210
+Email   : ashin8@myseneca.ca
+Section : NHH
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main(){
+    printf("+-------------------------------+\n");
+    printf("| Introduction to C Programming |\n");
+    printf("+-------------------------------+\n");
+    printf("Program written by:\n");
+    printf("ANNA SHIN\n");
+    return 0;
+}
+
+/* or printf like this. Below code also show the same output. But editing is difficult to programmer.
+printf("+-------------------------------+\n | Introduction to C Programming |\n +-------------------------------+\n Program written by:\n ANNA SHIN \n");
+*/
