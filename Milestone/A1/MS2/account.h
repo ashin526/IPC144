65f7bcf7d8128629e1a92f4e650569d9c1229fb6
Anna Shin <anna.shin526@gmail.com>
2022-04-26 02:43:48 -0400

    Assignment 1 - Milestone 2

---
+++ Milestone/A1/MS2/account.h
@@ -0,0 +1,30 @@
+// #############################################################################################
+//  Assignment: 1 
+//  Milestone : 2
+// #############################################################################################
+//  Student Name  : Anna Shin
+//  Student ID    : 131283210
+//  Student Email : ashin8@myseneca.ca 
+//  Course Section: NHH
+// #############################################################################################
+
+// Define a Macro
+#include<stdio.h>
+
+// STRUCTURES
+struct Person{
+char fullname[31];
+int birth;
+double income;
+char country[31];
+};
+
+struct UserLogin{
+char login[11];
+char password[9];
+};
+
+struct Account{
+int account_no;
+char account_type;
+};
